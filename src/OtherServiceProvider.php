05ef0633975822c434f962340f2f0b4a78870731
liuchengguo <liuchengguo@we-te.com>
2016-11-18 14:54:31 +0800

    no message

---
+++ src/OtherServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Liuchengguos\Other;
+
+use Illuminate\Support\ServiceProvider;
+
+class OtherServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('liuchengguos/other');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('other', function()
+        {
+            return new Other;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+}
